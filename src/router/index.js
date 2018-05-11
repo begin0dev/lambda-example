ab76c17d646ddd29eb8e18d2c093fb2f267752d2
beginner <prosonic1@naver.com>
2018-05-11 10:49:59 +0900

    serverless version v1.0 => v1 으로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
-const versions = {
-  '1.0': require('./v1.0'),
-};
+const v1 = require('./v1');
 
 const router = express.Router();
 
-router.use('/v1.0', versions['1.0']);
+router.use('/v1', v1);
 
 module.exports = router;
